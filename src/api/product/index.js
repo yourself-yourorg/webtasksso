2658ec89dbab4c89f0bd38113f2a5cac69c3a052
Martin Bramwell <martinhbramwell@gmail.com>
2018-06-03 19:06:10 -0400

    Enhance (sales): Adds a list of products
    Signed-off-by: Martin Bramwell <martinhbramwell@gmail.com>

--- src/api/product/index.js
+++ src/api/product/index.js
@@ -11,7 +11,7 @@ const MODULE = 'product';
 // const payload = '8CUj01QVX9tDqUuE%2FwE3CPEPqWi%2FG85iIfjf71%2Fv61eZuFEdwMjl7tTxsOIqom7N26Q6Og%3D%3D';
 
 const doList = (req, res, next, mode) => {
-  LG(`\n\n\n\n******* Listing all ${MODULE}s ******\n\n\n`);
+  LG(`\n\n\n\n******* Listing (${mode}) all ${MODULE}s ******\n\n\n`);
   LG(req.params);
   let urltext = wrapParams(req, mode);
   // LG(`'testGetPage', 'encoded ciphertext', ${payload} payload` );
@@ -20,13 +20,18 @@ const doList = (req, res, next, mode) => {
   LG(call);
   (new Backend).get(call, (data, response) => {
     LG(`********** Last ${req.params.module} *********`);
-    const list = data[req.params.module].data;
-    LG(list[list.length - 1]);
-    LG('.............');
-    // LG(data[req.params.module].enums);
-    // LG(data);
-    // LG(urltext);
-
+    if(data.error) {
+      LG(`************* FAILED ************`);
+      LG(data.error);
+    } else {
+      const list = data[req.params.module].data;
+      LG(list[list.length - 1]);
+      LG('.............');
+      // LG(data[req.params.module].enums);
+      // LG(data);
+      // LG(urltext);
+      LG(`************* sending ************`);
+    };
     res.json(data);
     LG(`************* sent ************`);
     return;
@@ -36,5 +41,5 @@ const doList = (req, res, next, mode) => {
 
 
 export default {
-  LIST: (req, res, next, mode) => doList(req, res, next, mode),
+  LIST: (req, res, next) => doList(req, res, next, 'list'),
 }
